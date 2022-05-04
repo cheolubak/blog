b359e29e30de5a59d4e5d283869d11bd33a34a92
B.Cheolu <cheolu.bak@sebasi.co.kr>
2022-05-04 18:29:29 +0900

    add submodules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './presentation/Home';
+import Notfound from './presentation/404';
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="404" element={<Notfound />} />
+			<Route path="*" element={<Navigate to="404" />} />
 		</Routes>
 	);
 };
---
+++ src/presentation/404/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Notfound() {
+	return <h1>Not found this page.</h1>;
+}
+
+export default Notfound;
